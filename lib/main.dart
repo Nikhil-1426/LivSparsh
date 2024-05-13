13bb1330b57f753cfdd887921c05f131422fa15d
Nikhil-1426 <parkar.nikhil1426@gmail.com>
2024-05-13 22:42:52 +0530

    Testing

--- lib/main.dart
+++ lib/main.dart
@@ -18,24 +18,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// import 'package:flutter/material.dart';
-// import 'loading_screen.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'My Flutter App',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//         visualDensity: VisualDensity.adaptivePlatformDensity,
-//       ),
-//       home: LoadingScreen(), // Directly going to LoadingScreen
-//     );
-//   }
-// }
